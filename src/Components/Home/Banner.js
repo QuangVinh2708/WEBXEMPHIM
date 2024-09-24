ee3090ba68fce94cf3b004c3688be04fef780259
quangvinh <quangvinhh2004@gmail.com>
2024-09-24 14:30:04 +0700

    quangvinh

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import PopularMovies from './PopularMovies';
 import { Movies } from './../../Data/MovieData';
+import FlexMovieItems from '../FlexMovieItems';
 
 function Banner() {
     return (
@@ -19,11 +20,16 @@ function Banner() {
                     <SwiperSlide key={index} className="relative rounded overflow-hidden">
                         <img
                             src={`${movie.image}`} // Ensure images are in public/images/movies/
-                            alt={movie.title}
+                            alt={movie.name}
                             className="w-full h-full object-cover"
                         />
                         <div className="absolute linear-bg xl:pl-52 sm:pl-32 pl-8 top-0 bottom-0 right=0 left-0 flex flex-col justify-center lg:gap-8 md:gap-5 gap-4">
-
+                            <h1 className='xl:text-4xl truncate capitalize font-sans sm:text-2xl font-bold'>
+                                {movie.name}
+                            </h1>
+                            <div className='flex gap-5 items-center text-dryGray'>
+                                <FlexMovieItems movie={movie}/>
+                            </div>
                         </div>
                     </SwiperSlide>
                 ))}
