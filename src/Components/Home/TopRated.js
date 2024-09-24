6d5ba26259ba85ffd4d209185de3cf6be278bdb4
ThuanPhat-design <phattran09082004@gmail.com>
2024-09-24 17:36:16 +0700

    ThuanPhat- file TopRated loi

--- src/Components/Home/TopRated.js
+++ src/Components/Home/TopRated.js
@@ -6,35 +6,37 @@ import {Movies} from '../../Data/MovieData';
 import Titles from './Titles';
 
 function TopRated() {
-    const [nextEl, setNextEl] = useState(null);
-    const [prevEl, setPrevtEl] = useState(null);
+    // const [nextEl, setNextEl] = useState(null);
+    // const [prevEl, setPrevtEl] = useState(null);
     return(
-        <div className='my-16'>
-            <Titles title='Top Rated' Icon={BsBookmarkStarFill} />
-            <div className="mt=10">
-                <Swiper 
-                navigation={{ nextEl, prevEl}} 
-                slidesPerView={4} 
-                spaceBetween={40} 
-                autoplay={true} 
-                speed={1000}
-                loop={true}
-                modules={[Navigation,Autoplay]}
-                >
-                    {
-                        Movies.map((movie, index) => (
-                            <SwiperSlide key={index}>
-                                <div className="p-4 h-rate border border-border bg-dry rounded-lg overflow-hidden">
-                                    <img 
-                                    src={`/images/movies/${movie.ttileImage}`}
-                                    alt={movie.name}
-                                    className="w-full h-full object-cover rounded-lg"
-                                    />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                </Swiper>
-            </div>
+        // <div className='my-16'>
+        //     <Titles title='Top Rated' Icon={BsBookmarkStarFill} />
+        //     <div className="mt=10">
+        //         <Swiper 
+        //         navigation={{ nextEl, prevEl}} 
+        //         slidesPerView={4} 
+        //         spaceBetween={40} 
+        //         autoplay={true} 
+        //         speed={1000}
+        //         loop={true}
+        //         modules={[Navigation,Autoplay]}
+        //         >
+        //             {
+        //                 Movies.map((movie, index) => (
+        //                     <SwiperSlide key={index}>
+        //                         <div className="p-4 h-rate border border-border bg-dry rounded-lg overflow-hidden">
+        //                             <img 
+        //                             src={`/images/movies/${movie.ttileImage}`}
+        //                             alt={movie.name}
+        //                             className="w-full h-full object-cover rounded-lg"
+        //                             />
+        //                         </div>
+        //                     </SwiperSlide>
+        //                 ))}
+        //         </Swiper>
+        //     </div>
+        // </div>
+        <div>Lát nhớ xóa
         </div>
     )
 }
